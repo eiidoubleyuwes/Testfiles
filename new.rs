a1225f218c4a17709613a792bdc04fcad5aa661e
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-17 23:48:53 +0300

    Feat:Simple
    I dont really care about this repo

--- new.rs
+++ new.rs
@@ -1,3 +1,14 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+fn main() {
+    let file_name = "rusty_files.txt";
+    let mut file = File::create(file_name).expect("Failed to create file");
+
+    // Write content to the file
+    let content = "This is a new file created by Rust";
+    file.write_all(content.as_bytes())
+        .expect("Failed to write to file");
+
+    println!("New file '{}' created successfully!", file_name);
+}
